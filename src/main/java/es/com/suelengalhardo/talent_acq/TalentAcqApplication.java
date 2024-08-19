2178493728905ef97e940fe3507eddf34a22b836
SuelenGalhardo <suelenranucci@gmail.com>
2024-08-19 21:19:03 +0200

    added tamplate springBoot

---
+++ src/main/java/es/com/suelengalhardo/talent_acq/TalentAcqApplication.java
@@ -0,0 +1,13 @@
+package es.com.suelengalhardo.talent_acq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentAcqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentAcqApplication.class, args);
+	}
+
+}
